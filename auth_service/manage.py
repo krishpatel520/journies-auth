8845b56e92dd773691141f9439eee28a6f29f859
hiren.dave <hiren.dave@msbcgroup.com>
2025-10-21 17:38:48 +0530

    Fixed routing and port

--- auth_service/manage.py
+++ auth_service/manage.py
@@ -2,9 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
+    port = os.getenv('PORT', '8000')
+    base_route = os.getenv('BASE_ROUTE', '')
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_service.settings')
     try:
@@ -15,6 +18,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) == 1:
+        # Default to runserver using env-defined port
+        sys.argv += ['runserver', f'0.0.0.0:{port}']
+    print(f"Starting Django app with:")
+    print(f"Full URL: http://127.0.0.1{base_route}\n")
     execute_from_command_line(sys.argv)
 
 
